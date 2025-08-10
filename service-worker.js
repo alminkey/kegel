62ce2b5dc0c4e5db0893d577b232ed86ca8108d1
alminkey <dnevnitk@gmail.com>
2025-08-10 12:18:59 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 
-const CACHE = 'kegelpilot-pwa-v38-2b';
+const CACHE = 'kegelpilot-pwa-v38-2c';
 const PRECACHE = ['./','./index.html','./manifest.json','./icons/kegelpilot_192.png','./icons/kegelpilot_512.png','./Logo.PNG','./assets/logo_text.png','./art/card-level.svg','./art/card-edu.svg','./art/card-quick.svg'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(PRECACHE)));self.skipWaiting();});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim();});
